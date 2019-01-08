64362ecd41b6229fc9f2836e1c31ce3d5b438d65
Pramod <prkrim@gmail.com>
2019-01-08 13:39:18 +0530

    Repo init

---
+++ src/HostedSites/Services/HostedSitesServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace HostedSites\Services;
+
+use Illuminate\Support\ServiceProvider;
+
+class HostedSitesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+
+    public function register()
+    {
+        //
+    }
+}
